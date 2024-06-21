554aeea8f90d7694dc98d9b20983eb8aa0bdf8e5
Staci Amburgey <Staci.Amburgey@dfw.wa.gov>
2024-06-21 22:29:48 +0000

    Update summary with 20th percentile

--- Scripts/cmdstan_sumstats.r
+++ Scripts/cmdstan_sumstats.r
@@ -49,4 +49,4 @@
 ## RSTAN
 
 # sumstats <- as.data.frame(summary(fit)$summary)
-sumstats <- summarise_draws(subset_draws(as_draws(fit),chain=keepchains), "mean","median","sd","mad", ~quantile(.x, probs = c(0.025,0.5,0.975)),"rhat")
+sumstats <- summarise_draws(subset_draws(as_draws(fit),chain=keepchains), "mean","median","sd","mad", ~quantile(.x, probs = c(0.025,0.20,0.5,0.975)),"rhat")
